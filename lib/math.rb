ca520a11e4e8278be59e4e303e33aa49df957cb7
Aubr R <a.renault2019@outlook.com>
2019-10-22 00:07:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,14 @@
 def addition(num1, num2)
-
+    puts num1+num2
+    return addition
 end
 
 def subtraction(num1, num2)
-
+    puts num1-num2
 end
 
 def division(num1, num2)
-
+    puts num1/num2
 end
 
 def multiplication(num1, num2)
